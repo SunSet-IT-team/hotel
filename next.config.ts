f33a90dc131d1ce6885016f030c7336645b573dd
At1set <iliya9874329@gmail.com>
2025-08-27 19:11:48 +0300

    feat/global scss styles

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-    output: 'standalone',
-};
+  output: "standalone",
+  sassOptions: {
+    // additionalData: `@use "styles/variables" as *;`,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
