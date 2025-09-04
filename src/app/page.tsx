2460d4ca0b8b750102fef0271015251bab1d85ba
Vlad <zhln.v3@gmail.com>
2025-09-04 14:08:58 +0300

    fix: clear page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,5 @@
-import styles from "./page.module.scss"
-import { Header } from "@/widgets/header/ui/Header"
-import { Button, Typography } from "@/shared/ui"
+import styles from "./page.module.scss";
 
 export default function Home() {
-  return (
-    <div className={styles.root}>
-      <Header variant="withSolidBg" className={styles.header}></Header>
-
-      <Header variant="withBgImage" className={styles.header}>
-        <Typography variant="h1" as="h1" color="white" className={styles.title}>
-          Открой мир и путешествуй легко
-        </Typography>
-        <div className={styles.buttonsBox}>
-          <Button variant="white" size="big">
-            Город или отель
-          </Button>
-          <Button variant="white" size="big">
-            Дата заезда
-          </Button>
-          <Button variant="white" size="big">
-            Дата выезда
-          </Button>
-          <Button variant="white" size="big">
-            Кол-во гостей
-          </Button>
-          <Button size="big">Поиск</Button>
-        </div>
-      </Header>
-    </div>
-  )
+  return <div className={styles.root}></div>;
 }
