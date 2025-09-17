d6f425ea888d6abba2aa186f71c1e1d1f8b20a37
At1set <iliya9874329@gmail.com>
2025-09-18 02:34:47 +0300

    chore: added index.ts to shared/hooks

---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useAppDispatch, useAppSelector } from './redux';
+export { useMediaQuery } from './useMediaQuery';
+export { useIsMobile } from './useMediaQuery';
+export { useOutsideClick } from './useOutsideClick';
