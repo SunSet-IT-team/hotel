8f190684f8fb4d679dc3b3e1588348c5be6d6ebb
Vlad <zhln.v3@gmail.com>
2025-09-17 21:26:02 +0300

    feature: add button props as

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -1,49 +1,73 @@
 'use client';
 
 import clsx from 'clsx';
-import { ButtonHTMLAttributes, FC } from 'react';
+import { AnchorHTMLAttributes, ButtonHTMLAttributes, FC, ReactNode } from 'react';
 
 import styles from './Button.module.scss';
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+type Variants = 'cyan' | 'white' | 'glass';
+type Sizes = 'small' | 'medium' | 'big';
+
+type CommonProps = {
     /**
      * Вариация кнопки
      */
-    variant?: 'cyan' | 'white' | 'glass';
+    variant?: Variants;
 
     /**
      * Размер кнопки (влияет на внутренние отступы и размер шрифта)
      */
-    size?: 'small' | 'medium' | 'big';
+    size?: Sizes;
 
     /**
      * Растягивать ли компонент на всю ширину родителя
      */
     fullWidth?: boolean;
-}
 
-/**
- * Компонент кнопки UI-кита
- */
-export const Button: FC<Props> = ({
-    variant = 'cyan',
-    size = 'medium',
-    fullWidth = false,
-    className,
-    ...rest
-}) => {
+    children?: ReactNode;
+    className?: string;
+};
+
+type ButtonAsAnchor = CommonProps & { as: 'a' } & AnchorHTMLAttributes<HTMLAnchorElement>;
+
+type ButtonAsButton = CommonProps & { as?: 'button' } & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export type Props = ButtonAsButton | ButtonAsAnchor;
+
+export const Button: FC<Props> = (props) => {
+    const {
+        variant = 'cyan',
+        size = 'medium',
+        fullWidth = false,
+        className,
+        children,
+        as = 'button',
+        ...rest
+    } = props as Props & { as: 'button' | 'a' };
+
+    const rootClassName = clsx(
+        styles.root,
+        styles[variant],
+        styles[size],
+        {
+            [styles.fullWidth]: fullWidth,
+        },
+        className,
+    );
+
+    if (as === 'a') {
+        const anchorProps = rest as AnchorHTMLAttributes<HTMLAnchorElement>;
+        return (
+            <a className={rootClassName} {...anchorProps}>
+                {children}
+            </a>
+        );
+    }
+
+    const buttonProps = rest as ButtonHTMLAttributes<HTMLButtonElement>;
     return (
-        <button
-            className={clsx(
-                styles.root,
-                styles[variant],
-                styles[size],
-                {
-                    [styles.fullWidth]: fullWidth,
-                },
-                className,
-            )}
-            {...rest}
-        />
+        <button className={rootClassName} {...buttonProps}>
+            {children}
+        </button>
     );
 };
