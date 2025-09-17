a220a53fcbd2dd0a13c8a1406cc8c96f9a7720a0
Vlad <zhln.v3@gmail.com>
2025-09-17 21:37:57 +0300

    chore: rename components folder to ui

--- src/features/searchLocation/index.ts
+++ src/features/searchLocation/index.ts
@@ -1,3 +1,3 @@
-export type { FetchData, Option } from '@/features/searchLocation/model/types';
-export { mockData as fetchMockData } from '@/features/searchLocation/story/mockData';
-export { SearchLocation } from '@/features/searchLocation/ui/SearchLocation';
+export type { FetchData, Option } from '@/features/SearchLocation/model/types';
+export { mockData as fetchMockData } from '@/features/SearchLocation/story/mockData';
+export { SearchLocation } from '@/features/SearchLocation/ui/SearchLocation';
