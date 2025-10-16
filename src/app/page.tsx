6926dc54b0d6361bc8cfc4153a9e7cc03f78fb65
Vlad <zhln.v3@gmail.com>
2025-10-16 18:07:46 +0300

    fix: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import { type FC } from 'react';
 import Image from 'next/image';
 
-import { mockHotels } from '@/entities/hotel';
 import HeaderBg from '@/shared/assets/img/header-bg.png';
-import { Container, Gallery, Typography } from '@/shared/ui';
 import { BookingButtons } from '@/widgets/BookingButtons';
 import { FaqSection } from '@/widgets/Faq';
 import { SearchForm } from '@/widgets/SearchForm';
@@ -25,16 +23,6 @@ const Home: FC = () => {
                 <SearchForm title="Открой мир и путешествуй легко" />
             </section>
             <BookingButtons className={styles.bookingButtons} />
-
-            {/* Секция с галереей популярных отелей */}
-            <section className={styles.gallerySection}>
-                <Container>
-                    <Typography variant="h1" color="dark" className={styles.galleryTitle}>
-                        Популярные направления
-                    </Typography>
-                    <Gallery images={mockHotels[0]?.images || []} alt="Популярные отели" />
-                </Container>
-            </section>
 
             <FaqSection />
         </main>
